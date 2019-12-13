87ce8589993b433cd88154afdae01639a2a7f540
benhicks1 <57331011+benhicks1@users.noreply.github.com>
2019-12-13 02:05:37 -0500

    Update ExampleQueries.sql

--- ExampleQueries.sql
+++ ExampleQueries.sql
@@ -1,8 +1,9 @@
-select * from victim 
-where weapon='unarmed' ;
-
 select * from shooting
 where county ='Philadelphia';
 
 select * from shooting 
 where legal=1;
+
+select vname, race, age from victim, shooting
+where weapon=0 and county='Philadelphia'
+group by vname, race, age;
