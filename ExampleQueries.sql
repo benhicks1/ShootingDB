1773c631b4c7b0ca9fdeae9810e5b95f09cc93ef
benhicks1 <57331011+benhicks1@users.noreply.github.com>
2019-12-13 02:53:45 -0500

    Update ExampleQueries.sql

--- ExampleQueries.sql
+++ ExampleQueries.sql
@@ -7,3 +7,13 @@ where legal=1;
 select vname, race, age from victim, shooting
 where weapon=0 and county='Philadelphia'
 group by vname, race, age;
+
+select name, officer.badge 
+from officer, shooting
+where legal=1 and officer.badge=shooting.badge
+group by name, officer.badge;
+
+select *
+from victim
+where gender='M' and race='W';
+
